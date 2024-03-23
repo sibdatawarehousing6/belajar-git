f7f19e1b6d2b57983e3039ce3770edac8bd486e8
abdulghif <abdulghif@gmail.com>
2024-03-23 10:26:24 +0700

    membangun class warga

---
+++ warga.py
@@ -0,0 +1,8 @@
+
+class Warga():
+    def __init__(self,NIK):
+        self.nik = NIK
+
+    def nyoblos(self,pilihan):
+        self.pilihan = pilihan
+        print(f'{self.nik} memilih {pilihan}')
